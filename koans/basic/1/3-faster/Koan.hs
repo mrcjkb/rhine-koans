447e68a788b78932a299968fba8931904b13a00d
Marc Jakobi <marc.jakobi@tiko.energy>
2024-11-25 22:07:23 +0100

    basic-1-3

--- koans/basic/1/3-faster/Koan.hs
+++ koans/basic/1/3-faster/Koan.hs
@@ -16,9 +16,7 @@ import FRP.Rhine
 
 -- Can you complete this?
 
-{- | The clock _type_ specifies the rate of the clock.
-type TenPerSecond = Millisecond _
--}
+type TenPerSecond = Millisecond 100
 
 -- | A clock that ticks 10 times second.
 tenPerSecond :: TenPerSecond
